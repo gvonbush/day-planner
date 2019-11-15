3b5329c3b71347817bbe154ba1e4f60b6dd51443
geeveebee <gvonbush@me.com>
2019-11-15 15:55:11 -0500

    built the hour blocks

---
+++ script.js
@@ -0,0 +1,31 @@
+//Need to create time blocks that represemnt 1 hour
+//Need to display standard time
+//blocks should include time, user input, and a save button
+//save button should save data to local storage
+//include document.ready
+$(document).ready(function(){
+    var hours=moment().hours(); // Number
+    var min=9
+    var max=17
+    
+    $(".time-block").each(function(){
+        var currentID=parseInt($(this).attr("id"))
+        console.log(currentID)
+        if (currentID < hours){
+            $(this).addClass("past")
+            
+        }
+        else if (currentID > hours){
+            $(this).addClass("future")
+        }
+        else if (currentID === hours){
+            $(this).addClass("present")
+        }
+    })
+    console.log(hours)
+})
+function updateLocalStorage(){
+
+}
+//save button to local storage$this
+//make "text area" in hours
