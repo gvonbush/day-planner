71a653de315579a0cbdd2949cc5d8ed958237bf1
geeveebee <gvonbush@me.com>
2019-11-16 16:54:45 -0500

    Added variables for all the text areas

--- script.js
+++ script.js
@@ -10,12 +10,12 @@ setInterval(function () {
     var hours = moment().hours(); // Number
     var min = 9
     var max = 17
-    
+
 
 
     $(".time-block").each(function () {
 
-        
+
         var currentID = parseInt($(this).attr("id"))
         // console.log(currentID)
         if (currentID < hours) {
@@ -44,9 +44,41 @@ $(document).ready(function () {
     var min = 9
     var max = 17
 
-    var savedData = localStorage.getItem("#text12")
-    console.log(savedData)
-    $("#text12").val(savedData)
+    var savedData9 = localStorage.getItem("#text9")
+    console.log(savedData9)
+    $("#text9").val(savedData9)
+
+    var savedData10 = localStorage.getItem("#text10")
+    console.log(savedData10)
+    $("#text10").val(savedData10)
+
+    var savedData11 = localStorage.getItem("#text11")
+    console.log(savedData10)
+    $("#text11").val(savedData11)
+
+    var savedData12 = localStorage.getItem("#text12")
+    console.log(savedData12)
+    $("#text12").val(savedData12)
+
+    var savedData13 = localStorage.getItem("#text13")
+    console.log(savedData13)
+    $("#text13").val(savedData13)
+
+    var savedData14 = localStorage.getItem("#text14")
+    console.log(savedData14)
+    $("#text14").val(savedData14)
+
+    var savedData15 = localStorage.getItem("#text15")
+    console.log(savedData15)
+    $("#text15").val(savedData15)
+
+    var savedData16 = localStorage.getItem("#text16")
+    console.log(savedData16)
+    $("#text16").val(savedData16)
+
+    var savedData17 = localStorage.getItem("#text17")
+    console.log(savedData17)
+    $("#text17").val(savedData17)
 
     // for (i=min; i<=max; i++){
     //     var timeCol = $("<div>").addClass("col-2")
@@ -79,16 +111,16 @@ $(document).ready(function () {
             $(this).addClass("present")
         }
     })
-    $(".saveBtn").click(function(){
+    $(".saveBtn").click(function () {
         console.log($(this).attr("text"))
         // Get text attribute from save button
-        var textID = "#"+$(this).attr("text")
+        var textID = "#" + $(this).attr("text")
         // Get the value from textarea with id = textID
         console.log($(textID).val())
         localStorage.setItem(textID, $(textID).val())
-       
 
-        
+
+
     })
 
     console.log(hours)
