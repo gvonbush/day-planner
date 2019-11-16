a011633b880eda26028729c2ccdb535918ce061a
geeveebee <gvonbush@me.com>
2019-11-16 12:25:46 -0500

    attempted to not have it static

--- script.js
+++ script.js
@@ -4,33 +4,75 @@
 //save button should save data to local storage
 //include document.ready
 var currentDay = $("#currentDay").text(moment().format("dddd, MMMM Do, h:mm A"));
-setInterval(function(){
-   currentDay = $("#currentDay").text(moment().format("dddd, MMMM Do, h:mm:ss A"));
-},1000)
+setInterval(function () {
+    currentDay = $("#currentDay").text(moment().format("dddd, MMMM Do, h:mm:ss A"));
+
+    var hours = moment().hours(); // Number
+    var min = 9
+    var max = 17
+    
+
+
+    $(".time-block").each(function () {
+
+        
+        var currentID = parseInt($(this).attr("id"))
+        console.log(currentID)
+        if (currentID < hours) {
+            $(this).addClass("past")
+            $(this).removeClass("future")
+            $(this).removeClass("present")
+
+        }
+        else if (currentID > hours) {
+            $(this).addClass("future")
+            $(this).removeClass("present")
+            $(this).removeClass("past")
+        }
+        else if (currentID === hours) {
+            $(this).addClass("present")
+            $(this).removeClass("future")
+            $(this).removeClass("past")
+        }
+    })
+
+}, 1000)
 console.log(currentDay.text())
 
-$(document).ready(function(){
-    var hours=moment().hours(); // Number
-    var min=9
-    var max=17
-    
-    $(".time-block").each(function(){
-        var currentID=parseInt($(this).attr("id"))
+$(document).ready(function () {
+    var hours = moment().hours(); // Number
+    var min = 9
+    var max = 17
+
+    for (i=min; i<=max; i++){
+        var timeCol = $("<div>").addClass("col")
+        var descrCol = $("<div>").addClass("col")
+        var saveCol = $("<div>").addClass("col")
+        var row = $("#"+i)
+        row.append(timeCol)
+        row.append(descrCol)
+        row.append(saveCol)
+        console.log(row)
+    }
+
+    $(".time-block").each(function () {
+        var currentID = parseInt($(this).attr("id"))
         console.log(currentID)
-        if (currentID < hours){
+        if (currentID < hours) {
             $(this).addClass("past")
-            
+
         }
-        else if (currentID > hours){
+        else if (currentID > hours) {
             $(this).addClass("future")
         }
-        else if (currentID === hours){
+        else if (currentID === hours) {
             $(this).addClass("present")
         }
     })
+
     console.log(hours)
 })
-function updateLocalStorage(){
+function updateLocalStorage() {
 
 }
 //save button to local storage$this
